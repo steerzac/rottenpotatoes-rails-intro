851ad777c33a72c7db365f2c9058d20cb97a70a4
steerzac <steerzac@gmail.com>
2019-01-16 16:08:42 +0000

    part 2 complete.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,26 +9,38 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
   def index
     @all_ratings = Movie.ratings
     @hilite = {:title => false, :rating => false, :release_date => false}
-    if params[:sort_by] == "Movie Title" then
-      @movies = Movie.order(:title) if params[:sort_by] == "Movie Title"
-      @hilite[:title] = true
-    elsif params[:sort_by] == "Rating" then
-      @movies = Movie.order(:rating)
-      @hilite[:rating] = true
-    elsif params[:sort_by] == "Release Date" then
-      @movies = Movie.order(:release_date)
-      @hilite[:release_date] = true
-    else
-      @movies = Movie.all
-    end 
+    @checked = {}
     
+    # Begin with all movies
+    @movies = Movie.all
+    
+    # Filter out those by ratings
     if params[:ratings] then
       @movies = @movies.where(:rating => params[:ratings].keys)
-    end
+      params[:ratings].keys.each do |rating|
+        @checked[rating] = true
+      end
+    else
+      @all_ratings.each do |rating|
+        @checked[rating] = true
+      end
+    end 
+    
+    # Sort remaining movies by column
+    if params[:sort_by] == "Movie Title" then
+      @movies = @movies.order(:title)
+      @hilite[:title] = true
+    elsif params[:sort_by] == "Rating" then
+      @movies = @movies.order(:rating)
+      @hilite[:rating] = true
+    elsif params[:sort_by] == "Release Date" then
+      @movies = @movies.order(:release_date)
+      @hilite[:release_date] = true
+    end 
   end
 
   def new
